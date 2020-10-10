6b636756eac25afc59d69b3515549d9ae95f62c3
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2020-10-10 15:43:39 +0200

    :tada: init strapi quickstart project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bca48eba3aa621c3eb7795172c577b38'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a39b8ee9-2629-4cef-b4a6-26eb0990ceea'
+};
